619f03c9151e5167e34b5d4467da4985058e3829
Austin Hester <ahester@incomm.com>
2022-06-21 20:00:27 +0000

    add 5 GB of storage to multipass instance for rust-builder
    Signed-off-by: Austin Hester <ahester57@gmail.com>

--- spin_up_rust_builder.sh
+++ spin_up_rust_builder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time multipass launch 20.04 --mem 4G --disk 20G --timeout 8000 --cloud-init solana-rust-builder.yaml -vvvv
+time multipass launch 20.04 --mem 4G --disk 25G --timeout 8000 --cloud-init solana-rust-builder.yaml -vvvv
